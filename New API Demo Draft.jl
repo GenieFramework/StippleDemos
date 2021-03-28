6aeed2e8fadfc1596d1bc9e0d081c53585f47b22
hhaensel <helmut.haensel@gmx.de>
2021-03-28 01:37:38 +0100

    adapt to API changes, toggle darkmode and header

--- New API Demo Draft.jl
+++ New API Demo Draft.jl
@@ -1,36 +1,60 @@
+using Revise
 using Stipple, StippleUI
 import Stipple: opts, OptDict
 Stipple.@kwdef mutable struct Example <: ReactiveModel
   name::R{String} = "Stipple!"
   firstname::String = "You"
-  name_private::String = "private"
+  name_::String = "readonly"
+  name__::String = "private"
   private::Private{String} = "Don't show"
   readonly::R{String} = "readonly", :readonly
   options::R{OptDict} = opts(f = js"()=>{}"), :jsfunction
   js::R{JSONText} = js"()=>{}", :jsfunction
+  header::R{Bool} = true
+  darkmode::R{Bool} = true
 end
 
+css() = style("""
+:root.stipple-blue body.body--dark{
+    --st-dashboard-module: #4e4e4e;
+    --st-dashboard-line: #a2a2a2;
+    --st-dashboard-bg: #616367;
+    --st-slider--track: #aab2b2;
+}
+.stipple-core body.body--dark{
+    --st-dashboard-bg: #616367;
+}
+""")
+
 model = Stipple.init(Example())
-Stipple.js_watch(Example) = """
-  js: function(val) { if (typeof(val)=="function") { val() } }
+Stipple.js_watch(model) = raw"""
+  js: function(val) { if (typeof(val)=="function") { val() } },
+  darkmode: function(val) { this.$q.dark.set(val) }
+"""
+
+Stipple.js_created(model) = raw"""
+  this.$q.dark.set(this.darkmode)
 """
 
 function ui()
   [
-    page(
+    dashboard(
       vm(model), class="container", title="Hello Stipple",
       [
-        h1([
-          "Hello, "
-          span("", @text(:name))
-        ])
-
+        template([
+          h1(["Hello, ", span("", @text(:name))])
+        ], v__if=:header)
+        row(cell(class="st-module", [
+          p(toggle("Camera on", fieldname = :header)),
+          p(toggle("Darkmode", :darkmode)),            
+        ]))
         p([
-          "What is your name? "
+          h1("What is your name? ")
           textfield("", :name, placeholder="type your name", label="Name", outlined="", filled="")
         ])
       ]
     )
+    css()
   ] |> html
 end
 
