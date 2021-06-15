a80002f1a2645f8c9b27c09a115178e25f295f4d
Yakir Gagnon <12.yakir@gmail.com>
2021-06-15 09:42:10 +0200

    use textfield instead of input

--- register.jl
+++ register.jl
@@ -3,10 +3,9 @@ using Stipple, StippleUI
 Base.@kwdef mutable struct RegistrationModel <: ReactiveModel
   name::R{String} = ""
   email::R{String} = ""
-  registered_email::R{Bool} = false
+  new_email::R{Bool} = true
   password::R{String} = ""
-  signup::R{Bool} = false
-  # disable::R{Bool} = true
+  submit::R{Bool} = false
 end
 
 model = Stipple.init(RegistrationModel())
@@ -16,50 +15,38 @@ email_db = Set(["a@b.com", "aa@bb.com"])
 
 # for each email, check to see if it's already registered in our database
 on(model.email) do email
-  model.registered_email[] = email ∈ email_db
+  model.new_email[] = email ∉ email_db
 end
 
 # these are the tests to see if input is valid or not, except for checking if an email is already registered or not, all of these are done on the client and not on the server: winning!
-Stipple.js_computed(model::RegistrationModel) = """
-  email_msg: function () {
-    if (this.email.length == 0) {
-      return 'email cannot be empty'
-    } else if (this.registered_email) {
-      return 'email already registered in our database'
-    } else {
-      return ''
+Stipple.js_methods(model::RegistrationModel) = """
+myRule (val) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(!val || this.new_email || 'email already registered in our database')
+    }, 1000)
+  })
+},
+onSubmit () {
+        this.\$q.notify({
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'cloud_done',
+          message: 'Submitted'
+        })
     }
-  },
-  name_msg: function () {
-    if (this.name.length == 0) {
-      return 'name cannot be empty'
-    } else {
-      return ''
-    }
-  },
-  password_msg: function () {
-    if (this.password.length == 0) {
-      return 'password cannot be empty'
-    } else {
-      return ''
-    }
-  }
 """
 
 function register()
   app = dashboard(vm(model),
                   [
                    heading("Registration"),
-                   row(cell(class="st-module", [
-                                                p(["Name", input("", placeholder="Type your name", @bind(:name), type = "text")]),
-                                                p([span("", @text(:name_msg))]),
-                                                p(["Email", input("", placeholder="Type your email", @bind(:email), type = "email")]),
-                                                p([span("", @text(:email_msg))]),
-                                                p(["Password", input("", placeholder="Type a password", @bind(:password), type = "password")]),
-                                                p([span("", @text(:password_msg))]),
-                                                p(btn("Sign up", @click(:signup), disable = :disable)),
-                                               ])),
-                  ], title = "Registration")
+                                                Stipple.form([
+                                                textfield("Your name *", :name, type = "text", filled = true, hint="Name and surname", lazy_rules = true, rules="[ val => val && val.length > 0 || 'Please type something' ]"),
+                                                textfield("Your email *", :email, type = "email", filled = true, hint="Email", lazy_rules = true, rules="[ val => val && val.length > 0 || 'Please type something']"), # add this if you want to have the server side checks for an already registered email:         , myRule ]"),
+                                                textfield("Password *", :password, type = "password", filled = true, hint="A password", lazy_rules = true, rules="[ val => val && val.length > 0 || 'Please type something' ]"),
+                                                btn("Sign up", @click("onSubmit"), type = "submit")
+                                               ]), ])
   html(app)
 end
 
