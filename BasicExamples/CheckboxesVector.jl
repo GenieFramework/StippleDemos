c1e48c6ddc7474340dc07ecff4fbb96a2f935f23
hhaensel <helmut.haensel@merckgroup.com>
2022-03-14 14:09:51 +0100

    add checkbox demo with mapping to BitVector

---
+++ BasicExamples/CheckboxesVector.jl
@@ -0,0 +1,34 @@
+using Stipple
+using StippleUI
+
+@reactive! mutable struct Model <: ReactiveModel
+    valone::R{Bool} = false
+    valtwo::R{Bool} = false
+    valthree::R{Bool} = false
+    checks::R{Vector{Bool}} = falses(3)
+    offsetchecks::R{OffsetVector{Bool, BitVector}} = OffsetArray(falses(3), -1)    
+end
+
+function ui(model)
+    page(model, class = "container", [
+        checkbox(label = "Apples", fieldname = :valone, dense = true),
+        checkbox(label = "Bananas", fieldname = :valtwo, dense = true),
+        checkbox(label = "Mangos", fieldname = :valthree, dense = true),
+        separator(),
+        [checkbox("Checkbox $i", Symbol("checks[$(i-1)]")) for i in 1:length(model.checks[])]...,
+        separator(),
+        [checkbox("Offset Checkbox $i", Symbol("offsetchecks[$i]")) for i in eachindex(model.offsetchecks[])]...
+    ])
+end
+
+model = Stipple.init(Model)
+
+route("/") do
+    html(ui(model), context = @__MODULE__)
+end
+
+up(async = true)
+
+model.valone[] = true
+model.checks[2] = true
+model.offsetchecks[2] = true
