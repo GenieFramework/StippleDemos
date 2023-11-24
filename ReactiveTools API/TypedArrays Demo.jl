ec32b236fc57f76552735ba5c58cf4f8ff78db00
Helmut Hänsel <helmut.haensel@gmx.com>
2023-11-24 19:02:11 +0100

    add TypedArrays Demo

---
+++ ReactiveTools API/TypedArrays Demo.jl
@@ -0,0 +1,65 @@
+# Download Demo
+
+using Stipple, Stipple.ReactiveTools
+using StippleUI
+
+import Stipple.opts
+import StippleUI.Tables.table
+
+# the next line might have to be adapted in the future
+pushfirst!(LOAD_PATH, abspath(joinpath(@__DIR__, "..", "Plugins")))
+using StippleTypedArrays
+using StippleDownloads
+
+@deps StippleTypedArrays
+
+@app begin
+    @in data = TypedArray(UInt8[])
+    @in data64 = TypedArray(UInt64[])
+
+    @in add_data = false
+    @in clear_data = false
+    @in hello_world = false
+
+    @onbutton add_data begin
+        x = rand(0:255)
+        push!(data.array, x)
+        notify(data)
+        push!(data64.array, x + 1000)
+        notify(data64)
+    end
+
+    @onbutton clear_data begin
+        data = data64 = []
+    end
+end
+
+function ui()
+    row(cell(class = "st-module q-ma-md", [
+      
+        row(class = "q-pa-md bg-green-2", "Data: [{{ data }}]")
+        row(class = "q-pa-md q-my-lg bg-green-4", "Data64: [{{ data64 }}]")
+
+        row([
+            btn("Add data", icon = "download", @click(:add_data), color = "primary", nocaps = true)
+            btn(class = "q-ml-lg", "Clear data", icon = "download", @click(:clear_data), color = "primary", nocaps = true)
+        ])
+    ]))
+end
+
+route("/") do
+    global model
+    model = @init
+    page(model, ui()) |> html
+end
+
+up()
+
+
+# other possible uses:
+
+# model.data[] = [1, 2, 3]
+# model.data[2] += 10
+
+# model.data64[] = [30, 20, 10]
+# model.data64[2] += 10
